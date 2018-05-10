4bbaf95e03c8b96ce460994f037b3bd660ac0efc
James Ge <jamesgeeze@gmail.com>
2018-05-10 00:21:47 -0700

    Prettyfied code

--- WIP/DriveTemplate.c
+++ WIP/DriveTemplate.c
@@ -10,13 +10,13 @@
 /*
 TODO:
 = add P (or PI) controller for stabilizing both sides of drive for encoder PID loop
-	+ http://www.robotc.net/wikiarchive/Tutorials/Arduino_Projects/Mobile_Robotics/VEX/Using_encoders_to_drive_some_distance
++ http://www.robotc.net/wikiarchive/Tutorials/Arduino_Projects/Mobile_Robotics/VEX/Using_encoders_to_drive_some_distance
 = add integral gain to
 = full pre-auton LCD selection screen
 = methods for moving robot forward and turning via encoderPIDController, static constant float for configuring ticks per inch
 = motor slew rate task:
-	+ https://www.vexforum.com/index.php/6146-robotc-programming-tips/p2#p53799
-	+ https://renegaderobotics.org/what-is-slew-rate/
++ https://www.vexforum.com/index.php/6146-robotc-programming-tips/p2#p53799
++ https://renegaderobotics.org/what-is-slew-rate/
 */
 
 /*
@@ -25,10 +25,11 @@ TODO:
 */
 static bool isTankDrive = false;
 static bool allowDriveToggle = true;
-
 /*
+============================================================================================================================
 Driving Task
 When buttons 5U and 7U are pressed at the same time, the drive can shift from tank drive to arcade drive
+============================================================================================================================
 */
 task toggleDrive(){
 	int left, right;
@@ -49,11 +50,6 @@ task toggleDrive(){
 }
 
 /*
-Proportional gain controller for drive motors.
-You can set desired encoder values using
-*/
-
-/*
 - Proportional gain constant
 - Requested values for encoders
 - Maximum motor power
@@ -64,7 +60,12 @@ static float  pidRequestedValueLB;
 static float  pidRequestedValueRB;
 static int maxEncoderPIDControllerPower = 70;
 static bool enableEncoderPIDController = true;
-
+/*
+============================================================================================================================
+Proportional gain controller for drive motors.
+You can set desired encoder values using
+============================================================================================================================
+*/
 task encoderPIDController()
 {
 	//intialize error and drive power vars
@@ -103,14 +104,14 @@ task encoderPIDController()
 	}
 }
 
-
 /*
 - Set ticks per inch on wheel
 */
 static int ticksPerInch = 29;
-
 /*
+============================================================================================================================
 Move robot forward
+============================================================================================================================
 */
 void moveForward(int tenthsOfInch, int power){
 	//YO YOU SHOULD REALLY TEST THIS
@@ -120,10 +121,11 @@ void moveForward(int tenthsOfInch, int power){
 	pidRequestedValueLB = tickGoal;
 	pidRequestedValueRB = -tickGoal;
 }
-
 /*
+============================================================================================================================
 Turn robot
-	+ http://www.robotc.net/wikiarchive/Tutorials/Arduino_Projects/Mobile_Robotics/VEX/Using_encoders_to_make_turns
+============================================================================================================================
++ http://www.robotc.net/wikiarchive/Tutorials/Arduino_Projects/Mobile_Robotics/VEX/Using_encoders_to_make_turns
 */
 void rotate(int degrees, int power){
 
@@ -133,9 +135,10 @@ void rotate(int degrees, int power){
 - Select an auton method
 */
 static int selectedAuton = 1;
-
 /*
-Pre-auton includes has an LCD menu that allows
+============================================================================================================================
+LCD program method
+============================================================================================================================
 */
 void lcdProgram(){
 	bLCDBacklight = true;
@@ -156,14 +159,14 @@ void lcdProgram(){
 	const short SensorMode = 0;
 	const short VarsMode = 1;
 	const short AutonMode = 2;
-	
+
 	displayLCDCenteredString (0, "Starting...");
 	wait1Msec(1000);
 	clearLCDLine(0);
 	clearLCDLine(1);
 
 	while (true){
-	//while (bIfiRobotDisabled == 1) {
+		//while (bIfiRobotDisabled == 1) {
 		select = false;
 
 		if (nLCDButtons == leftButton){
@@ -189,23 +192,23 @@ void lcdProgram(){
 				currentMode = 0;
 			}
 			switch(currentMode){
-				case SensorMode:
-					maxDisplays = maxSensorDisplays;
-					displayLCDCenteredString(0, "|Sensors|");
-					wait1Msec(500);
-					break;
-				case VarsMode:
-					maxDisplays = maxVarsDisplays;
-					displayLCDCenteredString(0, "|Global Vars|");
-					wait1Msec(500);
-					break;
-				case AutonMode:
-					maxDisplays = maxAutonDisplays;
-					displayLCDCenteredString(0, "|Auton Selector|");
-					wait1Msec(500);
-					break;
-				default:
-					//do nothing
+			case SensorMode:
+				maxDisplays = maxSensorDisplays;
+				displayLCDCenteredString(0, "|Sensors|");
+				wait1Msec(500);
+				break;
+			case VarsMode:
+				maxDisplays = maxVarsDisplays;
+				displayLCDCenteredString(0, "|Global Vars|");
+				wait1Msec(500);
+				break;
+			case AutonMode:
+				maxDisplays = maxAutonDisplays;
+				displayLCDCenteredString(0, "|Auton Selector|");
+				wait1Msec(500);
+				break;
+			default:
+				//do nothing
 			}
 		}
 		if (currentDisplay < 0){
@@ -215,111 +218,111 @@ void lcdProgram(){
 			currentDisplay = 0;
 		}
 		switch(currentMode){
-			case SensorMode:
-				switch(currentDisplay){
-					case 0:
-						string batteryLevel = nAvgBatteryLevel;
-						displayLCDCenteredString(0, "Battery Voltage");
-						displayLCDCenteredString(1, batteryLevel);
-						break;
-					case 1:
-						string le = SensorValue[leftEncoder];
-						displayLCDCenteredString(0, "Encoder LB");
-						displayLCDCenteredString(1, le);
-						break;
-					case 2:
-						string re = SensorValue(rightEncoder);
-						displayLCDCenteredString(0, "Encoder RB");
-						displayLCDCenteredString(1, re);
-						break;
-					case 3:
-						displayLCDCenteredString(0, "Sensor 4");
-						displayLCDCenteredString(1, "empty");
-						break;
-					default:
-						displayLCDCenteredString(0, "default case");
-						break;
+		case SensorMode:
+			switch(currentDisplay){
+			case 0:
+				string batteryLevel = nAvgBatteryLevel;
+				displayLCDCenteredString(0, "Battery Voltage");
+				displayLCDCenteredString(1, batteryLevel);
+				break;
+			case 1:
+				string le = SensorValue[leftEncoder];
+				displayLCDCenteredString(0, "Encoder LB");
+				displayLCDCenteredString(1, le);
+				break;
+			case 2:
+				string re = SensorValue(rightEncoder);
+				displayLCDCenteredString(0, "Encoder RB");
+				displayLCDCenteredString(1, re);
+				break;
+			case 3:
+				displayLCDCenteredString(0, "Sensor 4");
+				displayLCDCenteredString(1, "empty");
+				break;
+			default:
+				displayLCDCenteredString(0, "default case");
+				break;
+			}
+			break;
+		case VarsMode:
+			switch(currentDisplay){
+				//If select is true, change values
+			case 0:
+				displayLCDCenteredString(0, "var 1");
+				break;
+			case 1:
+				displayLCDCenteredString(0, "var 2");
+				break;
+			case 2:
+				displayLCDCenteredString(0, "var 3");
+				break;
+			case 3:
+				displayLCDCenteredString(0, "var 4");
+				break;
+			default:
+				displayLCDCenteredString(0, "default case");
+				break;
+			}
+			break;
+		case AutonMode:
+			switch(currentDisplay){
+			case 0:
+				if (select){
+					selectedAuton = 1;
+				}
+				if (selectedAuton == 1){
+					displayLCDCenteredString(0, "[Auton 1]");
+					} else {
+					displayLCDCenteredString(0, "Auton 1");
 				}
 				break;
-			case VarsMode:
-				switch(currentDisplay){
-					//If select is true, change values
-					case 0:
-						displayLCDCenteredString(0, "var 1");
-						break;
-					case 1:
-						displayLCDCenteredString(0, "var 2");
-						break;
-					case 2:
-						displayLCDCenteredString(0, "var 3");
-						break;
-					case 3:
-						displayLCDCenteredString(0, "var 4");
-						break;
-					default:
-						displayLCDCenteredString(0, "default case");
-						break;
+			case 1:
+				if (select){
+					selectedAuton = 2;
+				}
+				if (selectedAuton == 2){
+					displayLCDCenteredString(0, "[Auton 2]");
+					} else {
+					displayLCDCenteredString(0, "Auton 2");
+				};
+				break;
+			case 2:
+				if (select){
+					selectedAuton = 3;
+				}
+				if (selectedAuton == 3){
+					displayLCDCenteredString(0, "[Auton 3]");
+					} else {
+					displayLCDCenteredString(0, "Auton 3");
 				}
 				break;
-			case AutonMode:
-				switch(currentDisplay){
-					case 0:
-						if (select){
-							selectedAuton = 1;
-						}
-						if (selectedAuton == 1){
-							displayLCDCenteredString(0, "[Auton 1]");
-						} else {
-							displayLCDCenteredString(0, "Auton 1");
-						}
-						break;
-					case 1:
-						if (select){
-							selectedAuton = 2;
-						}
-						if (selectedAuton == 2){
-							displayLCDCenteredString(0, "[Auton 2]");
-						} else {
-							displayLCDCenteredString(0, "Auton 2");
-						};
-						break;
-					case 2:
-						if (select){
-							selectedAuton = 3;
-						}
-						if (selectedAuton == 3){
-							displayLCDCenteredString(0, "[Auton 3]");
-						} else {
-							displayLCDCenteredString(0, "Auton 3");
-						}
-						break;
-					case 3:
-						if (select){
-							selectedAuton = 4;
-						}
-						if (selectedAuton == 4){
-							displayLCDCenteredString(0, "[Auton 4]");
-						} else {
-							displayLCDCenteredString(0, "Auton 4");
-						}
-						break;
-					case 4:
-						if (select){
-							selectedAuton = 5;
-						}
-						if (selectedAuton == 4){
-							displayLCDCenteredString(0, "[No Auton]");
-						} else {
-							displayLCDCenteredString(0, "No Auton");
-						}
-						break;
-					default:
-						displayLCDCenteredString(0, "default case");
-						break;
+			case 3:
+				if (select){
+					selectedAuton = 4;
+				}
+				if (selectedAuton == 4){
+					displayLCDCenteredString(0, "[Auton 4]");
+					} else {
+					displayLCDCenteredString(0, "Auton 4");
+				}
+				break;
+			case 4:
+				if (select){
+					selectedAuton = 5;
+				}
+				if (selectedAuton == 4){
+					displayLCDCenteredString(0, "[No Auton]");
+					} else {
+					displayLCDCenteredString(0, "No Auton");
 				}
 				break;
 			default:
-				//do nothing
+				displayLCDCenteredString(0, "default case");
+				break;
+			}
+			break;
+		default:
+			//do nothing
 		}
 	}
 	clearLCDLine(0);
@@ -330,7 +333,11 @@ void lcdProgram(){
 task pre_auton(){
 	lcdProgram();
 }
-
+/*
+============================================================================================================================
+Auton methods
+============================================================================================================================
+*/
 void auton1(){
 }
 void auton2(){
@@ -339,6 +346,12 @@ void auton3(){
 }
 void auton4(){
 }
+
+/*
+============================================================================================================================
+Auton Task
+============================================================================================================================
+*/
 task autonomous(){
 	if (selectedAuton == 1){
 		auton1();
@@ -354,6 +367,11 @@ task autonomous(){
 	}
 }
 
+/*
+============================================================================================================================
+Main Task
+============================================================================================================================
+*/
 task main(){
 	//startTask(encoderPIDController);
 	//startTask(toggleDrive);
